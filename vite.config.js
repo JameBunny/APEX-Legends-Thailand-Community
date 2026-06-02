5bc4d25d64409fc6b5decf9e290df9e50dc4534a
James XO Arena <224217651+JameBunny@users.noreply.github.com>
2026-06-03 04:06:30 +0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// เปลี่ยน 'YOUR_REPOSITORY_NAME' เป็นชื่อคลังเก็บโค้ดของคุณบน GitHub เช่น 'apex-thailand'
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/YOUR_REPOSITORY_NAME/' : '/',
+})
